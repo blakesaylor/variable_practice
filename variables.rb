0435e77e8b923ee811c2dcc32a56c2f7ec96fd7c
Blake Saylor <blakesaylor@gmail.com>
2022-04-13 19:57:00 -0600

    Add comments and annotations

--- variables.rb
+++ variables.rb
@@ -2,19 +2,24 @@ first_name = "blake"
 last_name = "saylor"
 hair_color = "brown"
 
+# Add four integer values: siblings, dogs, cats, and computers
 siblings = 2
 dogs = 0
 cats = 0
+# Number of computers owned -- both laptops and desktops
 computers = 3
 
+# Add three float variables: checking_account, savings_account, and wallet
 checking_account = 512.36
 savings_account = 1250.75
 cash = 60.42
 
+# Add three boolean variables: employed, in_school, and insured
 employed = false
 in_school = true
 insured = true
 
+# Reassign string variables from my information to Jesse's
 first_name = "jesse"
 last_name = "burrow"
 hair_color = "brown"
