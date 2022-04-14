4a2c4da2c2c6424de67a38c8bf826f2ea20c0d28
Blake Saylor <blakesaylor@gmail.com>
2022-04-13 19:54:07 -0600

    Add integer variables

--- variables.rb
+++ variables.rb
@@ -2,6 +2,11 @@ first_name = "blake"
 last_name = "saylor"
 hair_color = "brown"
 
+siblings = 2
+dogs = 0
+cats = 0
+computers = 3
+
 checking_account = 512.36
 savings_account = 1250.75
 cash = 60.42
